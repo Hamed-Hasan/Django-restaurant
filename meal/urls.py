42b421137f73b1f06e3c18a74768d545015d0a12
Hamed Hasan <hamedhasan422421@gmail.com>
2023-12-09 15:27:51 +0300

    🚀 Added meal boxes for displaying Featured Items. 📊 Implemented card layout for tracking Item Details. 🌟 Updated card design with Good Comments display functionality. 🏆 Integrated Django URL patterns for showcasing selected items. 🔄 Refactored code structure in show_item_details view.

--- meal/urls.py
+++ meal/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
-    path('',views.show_item,name='show_item'),
+    path('', views.show_item, name='show_item'),
+    path('<str:item_id>/', views.show_item_details, name='show_item_details'),
 ]
